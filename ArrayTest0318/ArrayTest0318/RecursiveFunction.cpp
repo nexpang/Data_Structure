3825df9009c8de559ee06b3730801bf937474145
nexpang <rlarudgur123@gmail.com>
2021-04-07 00:50:30 +0900

    몇개 수정

--- ArrayTest0318/ArrayTest0318/RecursiveFunction.cpp
+++ ArrayTest0318/ArrayTest0318/RecursiveFunction.cpp
@@ -10,14 +10,16 @@ int main() {
 	cin >> a;
 	cout << "1부터" << a << "까지의 곱은 " << Factorial(a) <<endl;
 
-	cout << "\n2.가우스 : ";
-	a = 100;
-	cout << "1부터" << a << "까지의 합은 " << ((1+a)*a)/2 << endl;
 	a = 10;
-	cout << "\n3.재귀함수 사용 가우스 : ";
+	cout << "\n2.가우스계산법 : ";
 	cin >> a;
+	cout << "1부터" << a << "까지의 합은 " << ((1+a)*a)/2 << endl;
+
+	cout << "\n3.재귀함수 사용 가우스 1~100 : ";
+	a = 100;
 	cout <<"1부터"<<a<<"까지의 합은 "<<Gauss(a)<<endl;
 
+	a = 10;
 	cout << "\n4. 팩토리얼 : ";
 	cin >> a;
 	for (int i = 2; i <= a; i++) {
